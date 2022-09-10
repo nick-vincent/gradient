fc40f43fd866e5f6163e750ae49406c8a93f6361
Nick Vincent <the.nick.vincent@gmail.com>
2022-09-09 20:08:47 -0700

    Add react component bindings

---
+++ react-bindings/src/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@gradient/web-components';
+
+import { defineCustomElements } from '@gradient/web-components/loader';
+
+defineCustomElements();
+export const GradientButton = /*@__PURE__*/createReactComponent<JSX.GradientButton, HTMLGradientButtonElement>('gradient-button');
--- web-components/stencil.config.ts
+++ web-components/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'gradient',
   outputTargets: [
+    react({
+      componentCorePackage: '@gradient/web-components',
+      proxiesFile: '../react-bindings/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
